b873e4516da90d0ded8c2ca2e2deb8923f8c2ce8
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2023-09-16 11:19:01 +0800

    add fuzzer for msvc 2022.

--- cmake/sanitize.cmake
+++ cmake/sanitize.cmake
@@ -4,6 +4,14 @@ if(NOT MSVC)
 list(APPEND SANITIZE_FLAGS undefined leak)
 endif()
 
+if(MSVC)
+if(1929 LESS ${MSVC_VERSION})
+message(STATUS "Use msvc with vs 2022 or new, add fuzzer")
+list(APPEND SANITIZE_FLAGS fuzzer)
+endif()
+
+endif()
+
 foreach(FLAG ${SANITIZE_FLAGS})
 
 add_compile_options(-fsanitize=${FLAG})
