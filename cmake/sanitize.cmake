abb5c4f01827d8e2764abbd51723c58616a43962
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2023-09-16 03:00:42 +0000

    remove a memory sanitize for gcc.

--- cmake/sanitize.cmake
+++ cmake/sanitize.cmake
@@ -1,5 +1,5 @@
 
-set(SANITIZE_FLAGS "address,undefined,memory,leak")
+set(SANITIZE_FLAGS "address,undefined,leak")
 
 add_compile_options(-fsanitize=${SANITIZE_FLAGS})
 add_link_options(-fsanitize=${SANITIZE_FLAGS})
