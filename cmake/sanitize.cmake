a7e3d62812207039a99a3b9c23c217605d7ea68d
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2023-09-16 10:45:09 +0800

    add a lots flags.

--- cmake/sanitize.cmake
+++ cmake/sanitize.cmake
@@ -1,2 +1,5 @@
-add_compile_options(-fsanitize=address)
-add_link_options(-fsanitize=address)
+
+set(SANITIZE_FLAGS "address,undefined,memory,leak")
+
+add_compile_options(-fsanitize=${SANITIZE_FLAGS})
+add_link_options(-fsanitize=${SANITIZE_FLAGS})
--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
-int main(){
+#include <fmt/format.h>
+#include <iostream>
+#include <string>
+int main() {
+  std::cout << fmt::format("{}",std::move(std::string("good"))) << "\n";
+  int i;
+  auto pt = new int ;
+  std::cout << i + *pt <<"\n";
+  int arr[3]{};
+  std::cout << arr[2] <<"\n";
   return 0;
 }
