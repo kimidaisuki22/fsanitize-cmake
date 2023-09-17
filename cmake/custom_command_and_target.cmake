4569a0600be2483e82ad9a94bfd39798aa0d969b
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2023-09-17 11:46:35 +0800

    use main_dependency.

--- cmake/custom_command_and_target.cmake
+++ cmake/custom_command_and_target.cmake
@@ -1,2 +1,4 @@
 add_custom_command(OUTPUT a.txt COMMAND touch a.txt)
 add_custom_target(create_a DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/a.txt)
+
+add_custom_command(OUTPUT x.gls COMMAND glslc x.vert MAIN_DEPENDENCY ${CMAKE_CURRENT_SOURCE_DIR}/x.vert)
