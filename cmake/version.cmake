1d902b342dd1678d92ef2ce54fb1b126f39e5880
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2023-09-19 20:33:32 +0800

    add version configure.

---
+++ cmake/version.cmake
@@ -0,0 +1,2 @@
+
+configure_file(${CMAKE_CURRENT_LIST_DIR}/version.cpp.in ${CMAKE_CURRENT_BINARY_DIR}/version.cpp @ONLY)
---
+++ version.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#include <string_view>
+std::string_view get_project_version();
