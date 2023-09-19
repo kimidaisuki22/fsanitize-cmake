a6895949f6fa2a49ac451bb1e221c86b74687e05
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2023-09-19 19:30:16 +0800

    change md5sum into cmake.

--- cmake/custom_command.cmake
+++ cmake/custom_command.cmake
@@ -1,6 +1,6 @@
 add_custom_command(
     TARGET main POST_BUILD
-    COMMAND md5 $<TARGET_FILE:main> > main.md5
+    COMMAND ${CMAKE_COMMAND} -E md5sum $<TARGET_FILE:main> > main.md5
     BYPRODUCTS main.md5
     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
 )
