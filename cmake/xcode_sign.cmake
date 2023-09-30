67e02ad956ce62b5620b58808c432f5af210a760
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2023-09-30 21:30:42 +0800

    add apple code sign.

---
+++ cmake/xcode_sign.cmake
@@ -0,0 +1,4 @@
+
+  # Set the default signing identity and team ID to use for all targets
+  set(CMAKE_XCODE_ATTRIBUTE_DEVELOPMENT_TEAM   "ABC12345DE"    CACHE STRING "")
+  set(CMAKE_XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY "Mac Developer" CACHE STRING "")
