883f9ef4b86b804fd7bfe81d851fd57f9003e875
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2023-09-30 20:36:58 +0800

    add a git hash.

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,2 +1,10 @@
 
+
+find_package(Git REQUIRED)
+execute_process(
+    COMMAND ${GIT_EXECUTABLE} rev-parse HEAD
+    OUTPUT_VARIABLE GIT_HASH
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
 configure_file(${CMAKE_CURRENT_LIST_DIR}/version.cpp.in ${CMAKE_CURRENT_BINARY_DIR}/version.cpp @ONLY)
